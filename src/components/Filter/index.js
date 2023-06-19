c14e2b44aa1d0b73e6f7b103b573986c07f39645
DangTrieu02 <dangtrieu.cdht@gmail.com>
2023-06-19 17:43:28 +0700

    oke

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,27 +1,61 @@
 import React, { useState } from "react";
 import { links } from "../../assets/images-links";
 import "./styles.css";
+import Box from '@mui/material/Box';
+import Tabs, { tabsClasses } from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+
 
 function Filter({ selectedFilter, setSelectedFilter }) {
+
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <div className="filter-div">
-      {links.map((item, i) => (
-        <div
-          key={i}
-          className={`links-box ${i == selectedFilter && "selected-box"}`}
-          onClick={() => {
-            console.log("selecting key", i);
-            setSelectedFilter(i);
+      <Box
+        sx={{
+          flexGrow: 1,
+          maxWidth: { xs: 150, sm: 1200 },
+          bgcolor: 'background.paper',
+        }}
+      >
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant="scrollable"
+          scrollButtons
+          aria-label="visible arrows tabs example"
+          sx={{
+            [`& .${tabsClasses.scrollButtons}`]: {
+              '&.Mui-disabled': { opacity: 0.3 },
+            },
           }}
         >
-          <img src={item.imgSrc} className="links-img" />
-          <p
-            className={`links-label ${i == selectedFilter && "selected-label"}`}
-          >
-            {item.label}
-          </p>
-        </div>
-      ))}
+
+          {links.map((item, i) => (
+            <div
+              key={i}
+              className={`links-box ${i == selectedFilter && "selected-box"}`}
+              onClick={() => {
+                console.log("selecting key", i);
+                setSelectedFilter(i);
+              }}
+            >
+              <img src={item.imgSrc} className="links-img" />
+              <p
+                className={`links-label ${i == selectedFilter && "selected-label"}`}
+              >
+                 <Tab label={item.label}/>
+                
+              </p>
+            </div>
+          ))}
+        </Tabs>
+      </Box>
     </div>
   );
 }

fd48459e1ab53464fc9d65fd4f12f0f6851a146b
vithungf <tvhung260104@gmail.com>
2023-06-20 16:07:57 +0700

    update

--- src/page/homePage.js
+++ src/page/homePage.js
@@ -1,28 +1,32 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import Cards from '../components/Cards';
 import Filter from './../components/Filter/index';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllHome } from '../service/homeService';
 
-
 function HomePage() {
-    const [selectedFilter, setSelectedFilter] = useState(0);
-  const homes = useSelector(({ home }) => {
-    return home.list
-  })
-  const dispatch = useDispatch()
+    const homes = useSelector(({ home }) => {
+        return home.list
+    })
+    const dispatch = useDispatch()
 
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(getAllHome())
-    },[])
+    }, []);
 
+    const [selectedFilter, setSelectedFilter] = useState(0);
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <Filter
+                selectedFilter={selectedFilter}
+                setSelectedFilter={setSelectedFilter}
+            />
+            { homes && <Cards list={homes} />}
+        </>
+    )
 }
 
 export default HomePage

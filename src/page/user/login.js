6d40a48c2e24a05ba32e3b252de376f7cb570dba
vithungf <tvhung260104@gmail.com>
2023-06-20 08:49:48 +0700

    update

--- src/page/user/login.js
+++ src/page/user/login.js
@@ -64,7 +64,7 @@ export default function Login({setIsLogin}) {
               required
               fullWidth
               id="email"
-              label="Email Address"
+              label="User Name"
               name="email"
               autoComplete="email"
               autoFocus

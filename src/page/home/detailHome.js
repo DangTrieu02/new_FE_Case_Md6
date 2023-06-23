caf32ad7c78b7dfcc394260f30b0d75e67478476
DangTrieu02 <dangtrieu.cdht@gmail.com>
2023-06-23 13:43:13 +0700

    haha

--- src/page/home/detailHome.js
+++ src/page/home/detailHome.js
@@ -59,12 +59,6 @@ export default function DetailHome() {
                                         <span class="visually-hidden">Next</span>
                                     </button>
                                 </div>
-
-
-
-
-
-
                                 <div className="card-body p-1-9 p-xl-5">
                                     <div className="mb-4">
                                         <h3 className="h4 mb-0">{currentHome.user.fullName}</h3>
@@ -107,17 +101,9 @@ export default function DetailHome() {
                             <div className="ps-lg-1-6 ps-xl-5">
                                 <div className="mb-5 wow fadeIn">
                                     <div className="text-start mb-1-6 wow fadeIn">
-                                        <h2 className="h1 mb-0 text-primary">#About Me</h2>
+                                        <h3 className="h1 mb-0 text-primary">#About home</h3>
                                     </div>
                                     <p>
-                                        It is a long established fact that a reader will be distracted by
-                                        the readable content of a page when looking at its layout. The point
-                                        of using Lorem Ipsum is that it has a more-or-less normal
-                                        distribution of letters, as opposed to using 'Content here, content
-                                        here', making it look like readable English. Many desktop publishing
-                                        packages and web page editors now use Lorem Ipsum as their default
-                                        model text, and a search for 'lorem ipsum' will uncover many web
-                                        sites still in their infancy.
                                     </p>
                                     <p className="mb-0">
                                         There are many variations of passages of Lorem Ipsum available, but

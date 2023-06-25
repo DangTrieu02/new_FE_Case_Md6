23e18ff249d25c972304011e7441edff90a9c267
DragonRF <duonglong410@gmail.com>
2023-06-25 21:44:36 +0700

    add search methods

--- src/service/homeService.js
+++ src/service/homeService.js
@@ -1,72 +1,70 @@
 // homeService.js
-import {createAsyncThunk} from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import customAxios from "./api";
 
 export const getAllHome = createAsyncThunk("homes/findAll", async () => {
-    const res = await customAxios.get("homes/");
-    return res.data;
+  const res = await customAxios.get("homes/");
+  return res.data;
 });
 
-export const createHome = createAsyncThunk(
-    "homes/addHome",
-    async (data) => {
-        const res = await customAxios.post("homes/", data);
-        return res.data;
-    }
-);
+export const createHome = createAsyncThunk("homes/addHome", async (data) => {
+  const res = await customAxios.post("homes/", data);
+  return res.data;
+});
 
 export const getHomeByUser = createAsyncThunk(
-    `homes/getHomeByUser`,
-    async (id) => {
-        const res = await customAxios.get(`homes/user/${id}`);
-        console.log(res.data, id, "homeservice");
-        return res.data;
-    }
+  `homes/getHomeByUser`,
+  async (id) => {
+    const res = await customAxios.get(`homes/user/${id}`);
+    console.log(res.data, id, "homeservice");
+    return res.data;
+  }
 );
 
 export const getHomeById = createAsyncThunk(`homes/getHomeById`, async (id) => {
-    const res = await customAxios.put(`homes/${id}`);
-    return res.data;
+  const res = await customAxios.put(`homes/${id}`);
+  return res.data;
 });
 
 export const getHomeByCategory = createAsyncThunk(
-    `homes/getHomeByCategory`,
-    async (categoryId) => {
-        const res = await customAxios.get(`homes/categories/${categoryId}`);
-        return res.data;
-    }
+  `homes/getHomeByCategory`,
+  async (categoryId) => {
+    const res = await customAxios.get(`homes/categories/${categoryId}`);
+    return res.data;
+  }
 );
 
 export const getHomeByPrice = createAsyncThunk(
-    `homes/getHomeByPrice`,
-    async ({min, max}) => {
-        const res = await customAxios.get(`homes/price?min=${min}&max=${max}`);
-        return res.data;
-    }
+  `homes/getHomeByPrice`,
+  async ({ min, max }) => {
+    const res = await customAxios.get(`homes/price?min=${min}&max=${max}`);
+    return res.data;
+  }
 );
 
 export const getHomeByName = createAsyncThunk(
-    `homes/getHomeByName`,
-    async (name) => {
-        const res = await customAxios.get(`homes/filter?name=${name}`);
-        return res.data;
-    }
+  `homes/getHomeByName`,
+  async (nameHome) => {
+    const res = await customAxios.get(`homes/filter?nameHome=${nameHome}`);
+    console.log(res.data);
+    return res.data;
+  }
 );
 
 export const getHomeByStatus = createAsyncThunk(
-    `homes/getHomeByStatus`,
-    async (status) => {
-        const res = await customAxios.get(`homes/status?status=${status}`);
-        return res.data;
-    }
+  `homes/getHomeByStatus`,
+  async (status) => {
+    const res = await customAxios.get(`homes/status?status=${status}`);
+    return res.data;
+  }
 );
 
 export const getHomeByAddress = createAsyncThunk(
-    "homes/getHomeByAddress",
-    async (address) => {
-        const res = await customAxios.get(
-            `homes/find-by-address?address=${address}`
-        );
-        return res.data;
-    }
+  "homes/getHomeByAddress",
+  async (address) => {
+    const res = await customAxios.get(
+      `homes/find-by-address?address=${address}`
+    );
+    return res.data;
+  }
 );
